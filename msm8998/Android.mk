51df49ef78990e8cf6643810e55907e37cca1a33
Simao Gomes Viana <xdevs23@outlook.com>
2017-11-02 21:35:26 +0100

    msm8998: use BOARD_HAVE_BLUETOOTH_QCOM for libbt-vendor check
    Change-Id: I935456cc2dc6d6f0c933f8dc482760bc9499246d

--- msm8998/Android.mk
+++ msm8998/Android.mk
@@ -1,3 +1,3 @@
-ifeq ($(call is-vendor-board-platform,QCOM),true)
+ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
 include $(call all-named-subdir-makefiles,libbt-vendor)
 endif # is-vendor-board-platform
